5da4d5fac048c30fbffb83b640a26023aa24b509
wuilliam <wuilliam@33ff9044-5d53-4825-b48c-6ca11fb3ad87>
2013-03-12 14:47:47 +0000

    Agregando pagians faltantes y acomodando detalles

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -27,10 +27,10 @@ var app = {
 	// 'load', 'deviceready', 'offline', and 'online'.
 	bindEvents : function() {
 		// Activp cuando estare en el Telefono
-		// document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener('deviceready', this.onDeviceReady, false);
 
 		// Activo cuando esta en TESTPC
-		this.onDeviceReady();
+		// this.onDeviceReady();
 	},
 	// deviceready Event Handler
 	//
@@ -51,109 +51,6 @@ var app = {
 
 			// Iniciando sliders
 			app.doStartSliders();
-		})
-	},
-	// Validacion de formularios
-	validationsEvent : function() {
-		$(function() {
-			// Formulario de Login
-			$("#frm-login").validate({
-				submitHandler : function(e) {
-					// Si el form es valido
-					if ($("#frm-login").valid()) {
-						// Capturando la data
-						inputs = $("#frm-login").serializeArray()
-						username = inputs[0].value
-
-						// TODO: cifrar clave
-						password = inputs[1].value
-
-						// Haciendo el Login
-						app.doLogin(username, password);
-						$.mobile.changePage("index.html#principal");
-					}
-				},
-				messages : {
-					username : {
-						required : 'Usuario requerido',
-						email : "Debe ser un correo electrónico"
-					},
-					password : {
-						required : "Clave requerida"
-					}
-				}
-			});
-
-			// Formulario Perfil
-			$("#frm-perfil").validate({
-				submitHandler : function(e) {
-					// Si el form es valido
-					if ($("#frm-perfil").valid()) {
-						// Capturando la data
-						inputs = $("#frm-perfil").serializeArray()
-
-						// Guardando Perfil
-						app.doSaveProfile();
-						$.mobile.changePage("index.html#principal");
-					}
-				},
-				messages : {
-					email : {
-						required : 'Email es un campo requerido',
-						email : "Debe ser un correo electrónico"
-					}
-				}
-			});
-
-			// Formulario Configurar
-			$("#frm-configurar").validate({
-				submitHandler : function(e) {
-					// Si el form es valido
-					alert("Hola")
-					if ($("#frm-configurar").valid()) {
-						// Capturando la data
-						inputs = $("#frm-configurar").serializeArray()
-
-						// Guardando Perfil
-						app.doSaveSettings();
-						$.mobile.changePage("index.html#principal");
-					}
-				},
-				messages : {
-					show_nnews : {
-						required : 'Campo requerido',
-						numeric: "Debe ser entero"
-					}
-				}
-			});
-		})
-	},
-
-	//
-	// Funciones varias de la apliocacion
-	//
-
-	// Logeo
-	doLogin : function(username, password) {
-		$(function() {
-			// console.log("doLogin " + username + " " + password);
-			// Aqui va la consulta a la base de datos, hay que disenar el obj
-		})
-	},
-
-	// Guardando perfil
-	doSaveProfile : function() {
-		$(function() {
-			// console.log("Guardando el perfil");
-			// Aqui va la consulta a la base de datos, hay que disenar el obj
-		})
-	},
-
-	// Guardando configuraciones
-	doSaveSettings: function() {
-		$(function() {
-			console.log("Guardando el configuraciones");
-			// Aqui va la consulta a la base de datos, hay que disenar el obj
 		})
 	},
 
@@ -187,5 +84,172 @@ var app = {
 		$("#configurar").live('pageshow', function(event) {
 			$('#configurar .flexslider').flexslider(flexopts);
 		});
-	}
+		$("#todas").live('pageshow', function(event) {
+			$('#todas .flexslider').flexslider(flexopts);
+		});
+		$("#personalizadas").live('pageshow', function(event) {
+			$('#personalizadas .flexslider').flexslider(flexopts);
+		});
+		$("#registro").live('pageshow', function(event) {
+			$('#registro .flexslider').flexslider(flexopts);
+		});
+		$("#recuperar").live('pageshow', function(event) {
+			$('#recuperar .flexslider').flexslider(flexopts);
+		});
+	},
+	// Validacion de formularios
+	validationsEvent : function() {
+		$(function() {
+			// Formulario de Login
+			$("#frm-login").validate({
+				submitHandler : function(e) {
+					// Si el form es valido
+					if ($("#frm-login").valid()) {
+						// Capturando la data
+						inputs = $("#frm-login").serializeArray()
+						username = inputs[0].value
+
+						// TODO: cifrar clave
+						password = inputs[1].value
+
+						// Haciendo el Login
+						app.doLogin(username, password);
+						$.mobile.changePage("index.html#principal");
+					}
+				},
+				messages : {
+					username : {
+						required : 'Usuario requerido',
+						email : "Debe ser un correo electrónico"
+					},
+					password : {
+						required : "Clave requerida"
+					}
+				}
+			});
+
+			// Formulario de Nuevo Usuario
+			$("#frm-registro").validate({
+				submitHandler : function(e) {
+					// Si el form es valido
+					if ($("#frm-registro").valid()) {
+						// Capturando la data
+						inputs = $("#frm-registro").serializeArray()
+
+						// Guardando Perfil
+						app.doCreateUser();
+						$.mobile.changePage("index.html#main");
+					}
+				},
+				messages : {
+					email : {
+						required : 'Email es un campo requerido',
+						email : "Debe ser un correo electrónico"
+					}
+				}
+			});
+
+			// Formulario de Recuperar clave
+			$("#frm-recuperar").validate({
+				submitHandler : function(e) {
+					// Si el form es valido
+					if ($("#frm-recuperar").valid()) {
+						// Capturando la data
+						inputs = $("#frm-recuperar").serializeArray()
+
+						// Guardando Perfil
+						app.doRecoverPassword();
+						$.mobile.changePage("index.html#main");
+					}
+				},
+				messages : {
+					email : {
+						required : 'Email es un campo requerido',
+						email : "Debe ser un correo electrónico"
+					}
+				}
+			});
+
+			// Formulario Perfil
+			$("#frm-perfil").validate({
+				submitHandler : function(e) {
+					// Si el form es valido
+					if ($("#frm-perfil").valid()) {
+						// Capturando la data
+						inputs = $("#frm-perfil").serializeArray()
+
+						// Guardando Perfil
+						app.doSaveProfile();
+						$.mobile.changePage("index.html#principal");
+					}
+				},
+				messages : {
+					email : {
+						required : 'Email es un campo requerido',
+						email : "Debe ser un correo electrónico"
+					}
+				}
+			});
+
+			// Formulario Configurar
+			$("#frm-configurar").validate({
+				submitHandler : function(e) {
+					// Si el form es valido
+					if ($("#frm-configurar").valid()) {
+						// Capturando la data
+						inputs = $("#frm-configurar").serializeArray()
+
+						// Guardando Perfil
+						app.doSaveSettings();
+						$.mobile.changePage("index.html#principal");
+					}
+				},
+				messages : {
+					show_nnews : {
+						required : 'Campo requerido',
+						numeric : "Debe ser entero"
+					}
+				}
+			});
+		})
+	},
+
+	//
+	// Funciones varias de la apliocacion
+	//
+
+	// Logeo
+	doLogin : function(username, password) {
+		$(function() {
+			// console.log("doLogin " + username + " " + password);
+		})
+	},
+
+	// Guardando perfil
+	doSaveProfile : function() {
+		$(function() {
+			// console.log("Guardando el perfil");
+		})
+	},
+
+	// Guardando configuraciones
+	doSaveSettings : function() {
+		$(function() {
+			// console.log("Guardando el configuraciones");
+		})
+	},
+
+	// Guardando nuevo usuario
+	doCreateUser : function() {
+		$(function() {
+			// console.log("Creando el usuario");
+		})
+	},
+
+	// Recuperando clave
+	doRecoverPassword : function() {
+		$(function() {
+			// console.log("Recuperando clave");
+		})
+	},
 };
